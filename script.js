c86cc1f6f2b8d59d49f543e62141f093e8e00b1f
Rohan Shrestha <rohanxtha2060@gmail.com>
2024-02-08 17:17:03 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//DOM (document object model) manupilation is a frontend javaScript
+//that represents the structure of a document as a tree of object
+//html is a real DOM (it is tree like structure and it is in the form of object
+
+//4 pillars of DOM
+//1.selecting of an element
+//2.changing HTML
+//3.changing CSS
+//4.event listener
+
+//selecting of an element
+//show in console that you select
+var a = document.querySelector("h1")
+console.log(a)
+
+//changing html
+var a = document.querySelector("h1")
+a.innerHTML = "bye bye" //previous h1 is changed to new h1(bye bye)
+
